cc68da6020c7950e1b19f460df434ebe3ee4f902
Irine <irine.ng93@gmail.com>
2023-04-18 08:54:41 +0700

    styling for details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import parse from 'html-react-parser'
 import voucherData from '../voucherdata.json';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import imgBack from '../images/arrow.png';
 
 const Detail = () => {
     const [show, setShow] = useState(false);
@@ -55,33 +56,62 @@ const Detail = () => {
 
     return (
         <>
-        <div className="appBody">
+        <div>
+          <div className='appBody'>
+          <div className='py-3'>
+              <Link to={'/rewards'}>
+                  <div className='back-group'>
+                      <img src={imgBack} alt="Back" />
+                      Back to rewards
+                  </div>
+              </Link>
+              <h1 className='mt-2'>Redeem</h1>
+          </div>
+
+          <div className='float'>
             <img src={detail['img']} alt="deal" className="img-detail" />
-            <div className='text-center mt-1'>
-                <div className='bold'>${detail['amount']} eVoucher</div>
-                <div className='bold'>{detail['name']}</div>
-                <div className="points">{detail['points']} points</div>
+            <div className='detail-points-sect'>
+              <div className='detail-points-left-col'>
+                Points
+                <div><span>5</span> points</div>
+              </div>
+              <div className='detail-points-right-col'>
+                Validity
+                <div>22 Mar 2023 to 21 Jun 2023</div>
+              </div>
             </div>
-           
-            <div className='mt-3'>Valid at {detail['location']}</div>
+            <div className='how-to-sect'>
+              <div>How to redeem</div>
+              <p>Use your points to enjoy a $5 voucher at any Jollibean store</p>
 
-            <div className='bold mt-3'>Terms & Conditions</div>
-            {parse(detail['tnc'])}
-
-            <div>
-                <Button variant="primary" size="lg" className="solid-btn" onClick={handleShow}>Redeem</Button>
+              <div>Terms and conditions</div>
+              <p>Lorem ipsum dolor sit amet, consectr adipiscing elit aliquip exea
+sed do eiusmod tempor incididunt ut labore et dolore magna
+quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo</p>
             </div>
+          </div>
+          </div>
+          
+
+          <div className='redeem-sect'>
+            <Button size="lg" className="solid-btn" onClick={handleShow}>Redeem</Button>
+            <div>By clicking "Redeem Now", you have 15 minutes to complete the redemption before it expires.</div>
+          </div>
 
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Body>Redeem now?</Modal.Body>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Body>
+          <div className='bold'>Get This Reward!</div>
+          <div>Redeem with 5 points?</div>
+        </Modal.Body>
         <Modal.Footer>
+          <Button onClick={redeem} className="solid-btn">
+            Yes, Redeem Now
+          </Button>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="primary" onClick={redeem}>
-            Yes, Redeem Now
-          </Button>
+          
         </Modal.Footer>
       </Modal>
         </div>
